74edd649ae78c9bc3b337a87ee8389130d92d6c1
Pedro Solber Aldana <120376050+PedroSolber@users.noreply.github.com>
2023-03-05 14:52:48 -0300

    Add files via upload

---
+++ clase 10/js.js
@@ -0,0 +1,13 @@
+var name = ("Pedro" + " " + "Enrique")
+var surname = ("Solber" + " " + "Aldana")
+console.log(name + " " + surname);
+
+let number1 = 15;
+let number2 = 3;
+let suma = (number1 + number2);
+console.log(suma);
+let number3 = 2;
+console.log(suma + number3);
+
+
+
